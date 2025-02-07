0acfc7e08c6e7bdc26a407b9c97e54dc72ec468e
KYG17 <sjkyg17@gmail.com>
2025-02-07 10:40:38 +0900

    [KYK] filter 	- 어노테이션 방식 	- web.xml 방식

---
+++ lec03_filter/src/main/java/com/gn/common/filter/DataFilter.java
@@ -0,0 +1,55 @@
+package com.gn.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+//서블릿의 별명을 servletNames에 작성
+//별명이 receiveDataServlet인 서블릿을 필터해줄거에요!
+//@WebFilter(servletNames = "receiveDataServlet")
+//@WebFilter("/receive/*")
+//@WebFilter("/*")
+@WebFilter("/receive/data")
+public class DataFilter extends HttpFilter implements Filter {
+	
+	private static final long serialVersionUID = 1L;
+
+	public DataFilter() {
+		super();
+		System.out.println("[DataFilter] 기본 생성자 생성됨!");
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		System.out.println("[DataFilter] inin() : 필터 초기화");
+		
+	}
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+		System.out.println("[DataFilter] 요청 가로챔");
+		//chain.doFilter를 기준으로 위에는 요청을 가로챔
+		chain.doFilter(req, res);
+		//chain.doFilter를 기준으로 아래는 요청을 가로챔
+		System.out.println("[DataFilter] 응답 가로챔");	
+	}
+	
+	@Override
+	public void destroy() {
+		//필터가 필요없어질 때 실행
+		System.out.println("[DataFilter] 필터 종료");
+	}
+
+
+	
+
+}
---
+++ lec03_filter/src/main/java/com/gn/controller/ReceiveDataServlet.java
@@ -0,0 +1,35 @@
+package com.gn.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/receive/data")
+//name은 필터의 별명 , urlPatterns는 inform action
+//@WebServlet(name="receiveDataServlet",urlPatterns="/receive/data")
+public class ReceiveDataServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ReceiveDataServlet() {}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		System.out.println("--- 확인 ---");
+		String data = req.getParameter("test_data");
+		System.out.println("데이터 : " + data);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req,resp); // doGet 메소드 호출
+	}
+	
+	
+	
+}
