2e22e5a04ae99e9f96f0b923b962d7cb150f23f0
KYG17 <sjkyg17@gmail.com>
2025-02-10 22:22:47 +0900

    [KYK] 뻘짓

---
+++ lec00_practice/src/main/java/com/gn/cookie/CreateCookie.java
@@ -0,0 +1,39 @@
+package com.gn.cookie;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/createCookie")
+public class CreateCookie extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CreateCookie() {
+        super();
+        
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie c = new Cookie("user_name","user01");
+		c.setMaxAge(60*60*24);
+		response.addCookie(c);
+		response.sendRedirect("/");
+		System.out.println("연결확인!!");
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
---
+++ lec00_practice/src/main/webapp/index.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+    <%
+    	Cookie [] cookies = request.getCookies();
+    %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>쿠키</title>
+</head>
+<body>
+
+	<ul>
+		<li><a href="/createCookie">쿠키!!</a></li>
+	
+		<li>
+			<% 
+			String userId = "쿠키없음";
+			if(cookies != null){
+				for(Cookie c : cookies){
+					if("user_id".equals(c.getName())){
+						userId = c.getValue();
+					}
+				}
+			} %>
+			아이디 : <%= userId %>
+		</li>
+	
+		
+	</ul>
+
+</body>
+</html>
